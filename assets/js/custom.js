bc8a50120ec8137c5b5cf012d911e08712e5ca33
devavology <145644105+devavology@users.noreply.github.com>
2024-11-25 18:48:59 +0530

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
     var $moreText = $this.closest('.mediterranean-text').find('.moretext');
   
     $moreText.slideToggle();
-    if ($this.text() == "Read less") {
-      $this.text("Read More");
+    if ($this.text() == "Méně čtěte") {
+      $this.text("Přečtěte si více");
     } else {
-      $this.text("Read less");
+      $this.text("Méně čtěte");
     }
   });
 });
@@ -31,3 +31,31 @@ items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 
 
+
+/*-- Scroll Up/Down add class --*/
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       //âíèç
+	   $('.site-header').addClass('scrolling_down');
+	   $('.site-header').removeClass('scrolling_up');
+   } else {
+      // ââåðõ 
+	   $('.site-header').addClass('scrolling_up');
+	   $('.site-header').removeClass('scrolling_down');
+   }
+   lastScrollTop = st;
+});	
+
+
+
+
+
+
+
+
+  AOS.init({
+    duration: 1200,
+  })
+  
